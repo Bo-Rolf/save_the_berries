77c23e85d5de40716ccf0a6a6ba315de68fbf1de
Vilmer Hahne Lundqvist <vilmerha@chalmers.se>
2025-11-14 09:23:10 +0100

    klasser

--- my-app/src/main/java/com/mycompany/app/Projectile.java
+++ my-app/src/main/java/com/mycompany/app/Projectile.java
@@ -1,7 +1,12 @@
 package com.mycompany.app;
-
+import java.awt.geom.Point2D;
 public abstract class Projectile extends Entity {
     private double MoveSpeed;
     private int damage;
 
+    public Projectile(int health, String name, Point2D position, double MoveSpeed, int damage){
+        super(health, name, position);
+        this.MoveSpeed = MoveSpeed;
+        this.damage = damage;
+    }
 }
