5bb6947c694e026e11484d890adf1035bc40720f
Yu-ShinH <yushin_hua@hotmail.com>
2025-11-25 09:59:04 +0100

    Controller

--- my-app/src/main/java/com/mycompany/app/controller/Controller.java
+++ my-app/src/main/java/com/mycompany/app/controller/Controller.java
@@ -1,15 +1,50 @@
 package com.mycompany.app.controller;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.mycompany.app.Lawn;
+import com.mycompany.app.Tile;
 import com.mycompany.app.model.Model;
+import com.mycompany.app.model.entities.PeaShooter;
+import com.mycompany.app.model.entities.Plant;
+
 
 public class Controller {
 
-    private Model model;
+    private final Model model;
+    private final Lawn lawn;
+    private final Viewport viewport;
 
-    public Controller(Model model) {
+    public Controller(Model model, Lawn lawn, Viewport viewport) {
         this.model = model;
-        // Controller initialization code
+        this.lawn = lawn;
+        this.viewport = viewport;
     }
 
-    // Controller methods
+    public void handleInput(float startX, float startY, float tileWidth, float tileHeight) {
+        if (Gdx.input.justTouched()) {
+            float mouseX = Gdx.input.getX();
+            float mouseY = Gdx.input.getY();
+
+            Vector2 worldCoords = viewport.unproject(new Vector2(mouseX, mouseY));
+
+            if (worldCoords.x >= startX && worldCoords.x <= startX + tileWidth * lawn.getCols()
+                    && worldCoords.y >= startY && worldCoords.y <= startY + tileHeight * lawn.getRows()) {
+
+                int col = (int) ((worldCoords.x - startX) / tileWidth);
+                int row = (int) ((worldCoords.y - startY) / tileHeight);
+
+                Tile clickedTile = lawn.getTile(row, col);
+
+                if (clickedTile != null && clickedTile.placeable == null) {
+                    float x = startX + col * tileWidth + tileWidth / 2f;
+                    float y = startY + row * tileHeight + tileHeight / 2f;
+                    Plant newPlant = new PeaShooter(new Vector2(x, y), row, col);
+                     model.game.addPlant(newPlant);
+                    clickedTile.place(newPlant);
+                }
+            }
+        }
+    }
 }
