4b7a1146a967afa26df6b31fa927a2fc9e29966a
SixtenPaulsson <sixtenpaulsson@gmail.com>
2025-12-04 17:07:17 +0100

    glömde att lägga till sunspawner classen :(

---
+++ my-app/src/main/java/com/mycompany/app/Sunspawner.java
@@ -0,0 +1,37 @@
+//Sak som spawnar sol
+package com.mycompany.app;
+
+import java.util.Random;
+
+import com.mycompany.app.model.entities.Sun;
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.random.*;
+
+public class Sunspawner {
+    private final double sunCooldown = 15;
+    private double timeSinceLastSun = 0;
+    private final Random r = new Random();
+
+    public Sunspawner() {
+
+    }
+    public void update(double deltaTime) { //Räknar tid från förra solen
+        timeSinceLastSun +=deltaTime;
+    }
+
+    public boolean canSpawnSun() {
+        return timeSinceLastSun >= sunCooldown;
+    }
+
+    
+    public Sun spawnSun() {
+        if (canSpawnSun()){
+            timeSinceLastSun = 0;
+            Vector2 sunpos = new Vector2(this.r.nextInt(100,700),this.r.nextInt(100,500));
+            return new Sun(sunpos,25);
+        }
+        return null;
+
+    }
+}
